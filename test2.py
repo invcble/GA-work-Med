d5f26219ab6b44c72a9e0d1a4510cac3a4380423
invcble <imonbera13@gmail.com>
2024-05-15 00:31:33 -0400

    Update

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@ import pdfplumber
 import re
 import os
 import sys
+import openpyxl
 
 
 
@@ -38,10 +39,47 @@ section_headers = [
     "Communication About Pain",
     "Comm About Medicines",
     "Discharge Information",
-    "Care Transitions"
+    "Care Transitions",
+    "Comments",
+    "Patient Name:"
     ]
 
 start_index = filtered_text.find("Comm About Medicines")
 end_index = filtered_text.find("Discharge Information")
 
-print(filtered_text[start_index : end_index])
+# print(filtered_text[start_index : end_index])
+print(filtered_text)
+dict = {}
+
+for each in section_headers:
+    dict[each] = filtered_text.find(each)
+
+clean_dict = {}
+for each in dict:
+    if dict[each] != -1:
+        clean_dict[each] = dict[each]
+
+# for i in dict:
+#     print(dict[i], i)
+for i in clean_dict:
+    print(clean_dict[i], i)
+
+##### ahahahahaahaha process dictionary to add next value in it, then itarate main list and reference
+# list = list(clean_dict.items())
+# print('-' * 40)
+# # print(list[1][1])
+# # print(len(list))
+# for i in range(len(list)-1):
+#     print(list[i][0])
+#     print(f'{list[i][1]} to {list[i+1][1]}')
+
+# workbook = openpyxl.Workbook()
+# sheet = workbook.active
+
+# for i in range(len(list)-1):
+#     sheet.cell(row=5, column=i+5, value=list[i][0])
+#     sheet.cell(row=6, column=i+5, value=f'{list[i][1]} to {list[i+1][1]}')
+#     # print(list[i][0])
+#     # print(f'{list[i][1]} to {list[i+1][1]}')
+
+# workbook.save("Temp.xlsx")
