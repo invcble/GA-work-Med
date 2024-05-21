83fa16a4354edbf526d7310561ee1860818ad726
invcble <imonbera13@gmail.com>
2024-05-20 23:35:36 -0400

    Update

--- test2.py
+++ test2.py
@@ -20,66 +20,61 @@ filtered_text = wholeText.replace("© 2023 Press Ganey Associates LLC † Custom
 # print(filtered_text.find("Care Transitionss"))
 
 section_headers = [
-    "Background Questions",
-    "Admission",
-    "Room",
-    "Meals",
-    "Nurses",
-    "Tests and Treatments",
-    "Visitors and Family",
-    "Doctors",
-    "Discharge",
-    "Personal Issues",
-    "Overall Assessment",
-    "About You",
-    "Comm w/ Nurses",
-    "Response of Hosp Staff",
-    "Comm w/ Doctors",
-    "Hospital Environment",
-    "Communication About Pain",
-    "Comm About Medicines",
-    "Discharge Information",
-    "Care Transitions",
-    "Comments",
-    "Patient Name:"
+    ["Background Questions", 0, 0],
+    ["Admission", 0, 0],
+    ["Room", 0, 0],
+    ["Meals", 0, 0],
+    ["Nurses", 0, 0],
+    ["Tests and Treatments", 0, 0],
+    ["Visitors and Family", 0, 0],
+    ["Doctors", 0, 0],
+    ["Discharge", 0, 0],
+    ["Personal Issues", 0, 0],
+    ["Overall Assessment", 0, 0],
+    ["About You", 0, 0],
+    ["Comm w/ Nurses", 0, 0],
+    ["Response of Hosp Staff", 0, 0],
+    ["Comm w/ Doctors", 0, 0],
+    ["Hospital Environment", 0, 0],
+    ["Communication About Pain", 0, 0],
+    ["Comm About Medicines", 0, 0],
+    ["Discharge Information", 0, 0],
+    ["Care Transitions", 0, 0],
+    ["Comments", 0, 0],
+    ["Patient Name:", 0, 0]
     ]
 
 start_index = filtered_text.find("Comm About Medicines")
 end_index = filtered_text.find("Discharge Information")
 
 # print(filtered_text[start_index : end_index])
-print(filtered_text)
+# print(filtered_text)
 dict = {}
 
 for each in section_headers:
-    dict[each] = filtered_text.find(each)
+    if filtered_text.find(each) != -1:
+        dict[each] = filtered_text.find(each)
 
-clean_dict = {}
-for each in dict:
-    if dict[each] != -1:
-        clean_dict[each] = dict[each]
-
-# for i in dict:
-#     print(dict[i], i)
-for i in clean_dict:
-    print(clean_dict[i], i)
+for i in dict:
+    print(dict[i], i)
 
 ##### ahahahahaahaha process dictionary to add next value in it, then itarate main list and reference
-# list = list(clean_dict.items())
-# print('-' * 40)
-# # print(list[1][1])
+list = list(dict.items())
+print('-' * 40)
+# print(list[1][1])
 # # print(len(list))
-# for i in range(len(list)-1):
-#     print(list[i][0])
-#     print(f'{list[i][1]} to {list[i+1][1]}')
+# print(list)
+for i in range(len(list)-1):
+    print(list[i][0])
+    print(f'{list[i][1]} to {list[i+1][1]}')
 
-# workbook = openpyxl.Workbook()
-# sheet = workbook.active
+workbook = openpyxl.Workbook()
+sheet = workbook.active
 
-# for i in range(len(list)-1):
-#     sheet.cell(row=5, column=i+5, value=list[i][0])
-#     sheet.cell(row=6, column=i+5, value=f'{list[i][1]} to {list[i+1][1]}')
-#     # print(list[i][0])
-#     # print(f'{list[i][1]} to {list[i+1][1]}')
+for i in range(len(list)-1):
+    sheet.cell(row=5, column=i+5, value=list[i][0])
+    sheet.cell(row=6, column=i+5, value=f'{list[i][1]} to {list[i+1][1]}')
+    # print(list[i][0])
+    # print(f'{list[i][1]} to {list[i+1][1]}')
 
-# workbook.save("Temp.xlsx")
+workbook.save("Temp.xlsx")
